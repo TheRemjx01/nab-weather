344ec2edac4fc41887ec84f69910371902756d2a
Nghia Nguyen Trung <ngnguyen@axon.com>
2020-08-18 18:02:32 +0700

    Boilerplate for WithFetch testing

---
+++ src/components/with-fetch/WithFetch.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import WithFetch from './WithFetch';
+
+describe('<WithFetch />', () => {
+ const Component = () => 'Fetch';
+ const ErrorComponent = () => 'Error';
+ const LoadingComponent = () => 'loading';
+
+ it('should be able to render', () => {
+  
+ })
+})
